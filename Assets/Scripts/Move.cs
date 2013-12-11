f4654bda9afe238d481106e8e2088fae0c9a3bb6
David Neilsen <david@panmedia.co.nz>
2013-12-11 20:53:32 +1300

    Update

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float moveSpeed = 50;
+
+	public void Start () {
+	
+	}
+	
+	public void Update () {
+	
+	}
+
+}
